d0667ab60757e0e45b5fcbfaa68274897bf883e8
uzmoi <63585970+uzmoi@users.noreply.github.com>
2025-05-16 02:51:57 +0900

    add bat, difftastic, dua, eza, fd, fzf, helix and ripgrep

--- home.nix
+++ home.nix
@@ -5,5 +5,16 @@
   home.homeDirectory = "/home/uzmoi";
   home.stateVersion = "24.11";
 
+  home.packages = with pkgs; [
+    bat
+    difftastic
+    dua
+    eza
+    fd
+    fzf
+    helix
+    ripgrep
+  ];
+
   programs.home-manager.enable = true;
 }
