ba296195b01ea918212ddfff6e829fec37a066d9
uzmoi <63585970+uzmoi@users.noreply.github.com>
2025-05-19 23:49:18 +0900

    remove hello

--- flake.nix
+++ flake.nix
@@ -13,11 +13,6 @@
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
   in {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
     homeConfigurations."uzmoi" = home-manager.lib.homeManagerConfiguration {
       inherit pkgs;
 
