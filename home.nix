de269b6c94605b2d8dc2b215fd106eebf3083589
uzmoi <63585970+uzmoi@users.noreply.github.com>
2025-05-20 20:55:12 +0900

    add git

--- home.nix
+++ home.nix
@@ -17,4 +17,24 @@
   ];
 
   programs.home-manager.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = "uzmoi";
+    userEmail = "63585970+uzmoi@users.noreply.github.com";
+    aliases = {
+      graph = "log --oneline --graph --all";
+      ignore-files = "ls-files -io --exclude-standard";
+      unstage = "restore --staged";
+    };
+    difftastic.enable = true;
+    extraConfig = {
+      init.defaultBranch = "main";
+      fetch.prune = true;
+      rebase.autostash = true;
+      rerere.enabled = true;
+      push.autoSetupRemote = true;
+      push.useForceIfIncludes = true;
+    };
+  };
 }
